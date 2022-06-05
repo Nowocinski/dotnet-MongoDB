fa62f36505b44031b40a19b08d1e14e471977762
Nowocinski <vito24222@gmail.com>
2022-06-05 12:03:17 +0200

    Create UpsertRecord and DeleteRecord methods

--- MongoDBDemoApp/MongoDBDemo/Program.cs
+++ MongoDBDemoApp/MongoDBDemo/Program.cs
@@ -36,14 +36,22 @@ namespace MongoDBDemo
             //    });
             #endregion
             #region Query by id
-            var oneRecord = db.LoadRecordById<PersonModel>("Users", new Guid("69003fb4-c4fb-4570-b442-55db24963865"));
-            Console.WriteLine($"{oneRecord.Id} {oneRecord.FirstName} {oneRecord.LastName}");
-            if (oneRecord.PrimaryAddress != null)
-            {
-                Console.WriteLine($"{oneRecord.PrimaryAddress.City}");
-            }
+            //var oneRecord = db.LoadRecordById<PersonModel>("Users", new Guid("69003fb4-c4fb-4570-b442-55db24963865"));
+            //Console.WriteLine($"{oneRecord.Id} {oneRecord.FirstName} {oneRecord.LastName}");
+            //if (oneRecord.PrimaryAddress != null)
+            //{
+            //    Console.WriteLine($"{oneRecord.PrimaryAddress.City}");
+            //}
             #endregion
-
+            #region UpsertRecord (Replace or add record)
+            //var oneRecord = db.LoadRecordById<PersonModel>("Users", new Guid("69003fb4-c4fb-4570-b442-55db24963865"));
+            //oneRecord.DateOfBirth = new DateTime(1982, 10, 31, 0, 0, 0, DateTimeKind.Utc);
+            //db.UpsertRecord("Users", oneRecord.Id, oneRecord);
+            #endregion
+            #region delete
+            var oneRecord = db.LoadRecordById<PersonModel>("Users", new Guid("69003fb4-c4fb-4570-b442-55db24963865"));
+            db.DeleteRecord<PersonModel>("Users", oneRecord.Id);
+            #endregion
             Console.ReadLine();
         }
     }
@@ -55,6 +63,8 @@ namespace MongoDBDemo
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public AddressModel PrimaryAddress { get; set; }
+        [BsonElement("dob")]
+        public DateTime DateOfBirth { get; set; }
     }
 
     public class AddressModel
@@ -92,6 +102,22 @@ namespace MongoDBDemo
             var collection = this.db.GetCollection<T>(table);
             var filter = Builders<T>.Filter.Eq("Id", id);
             return collection.Find(filter).First();
+        }
+
+        // Replace or add record
+        public void UpsertRecord<T>(string table, Guid id, T record)
+        {
+            var collection = this.db.GetCollection<T>(table);
+            // https://stackoverflow.com/questions/67158337/c-sharp-mongodb-upsert-bsonvalueguid-is-obsolete-use-the-bsonbinarydata-c
+            BsonBinaryData binData = new BsonBinaryData(id, GuidRepresentation.Standard);
+            var result = collection.ReplaceOne(new BsonDocument("_id", binData), record, new ReplaceOptions { IsUpsert = true });
+        }
+
+        public void DeleteRecord<T>(string table, Guid id)
+        {
+            var collection = this.db.GetCollection<T>(table);
+            var filter = Builders<T>.Filter.Eq("Id", id);
+            collection.DeleteOne(filter);
         }
     }
 }
